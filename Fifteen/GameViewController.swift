f7fad737e0a0a46d99f7fc53c1d8140ff59a7538
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-11-13 17:02:23 +0200

    Initial

--- Fifteen/GameViewController.swift
+++ Fifteen/GameViewController.swift
@@ -30,23 +30,6 @@ class GameViewController: UIViewController {
         }
     }
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
